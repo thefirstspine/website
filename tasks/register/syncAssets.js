8282e25f8762361c9a712e252f205446783bc4c9
thefirstspine <teddy@coretizone.com>
2020-01-23 09:19:40 +0100

    Initial commit

---
+++ tasks/register/syncAssets.js
@@ -0,0 +1,16 @@
+/**
+ * `tasks/register/syncAssets.js`
+ *
+ * ---------------------------------------------------------------
+ *
+ * For more information see:
+ *   https://sailsjs.com/anatomy/tasks/register/sync-assets.js
+ *
+ */
+module.exports = function(grunt) {
+  grunt.registerTask('syncAssets', [
+    'less:dev',
+    'sass:dev',
+    'sync:dev',
+  ]);
+};
