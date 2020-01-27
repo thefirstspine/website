27388db0f5592ecaf1b2e986a0eb00dde5426152
thefirstspine <teddy@coretizone.com>
2020-01-27 19:37:13 +0100

    Added allowOrigins CORS for production

--- config/env/production.js
+++ config/env/production.js
@@ -148,9 +148,9 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     cors: {
-      // allowOrigins: [
-      //   'https://example.com',
-      // ]
+      allowOrigins: [
+        'http://beta.thefirstspine.fr',
+      ]
     },
 
   },
