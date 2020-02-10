2a68cc197a7c288721fc9a891646e80fec42b6b6
thefirstspine <teddy@coretizone.com>
2020-02-10 23:40:29 +0100

    Used process.env as done in boiler

--- api/helpers/layout-config.js
+++ api/helpers/layout-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   fn: function() {
     return {
-      solidPancakeUrl: sails.config.custom.dotenv.SOLID_PANCAKE_URL,
+      solidPancakeUrl: sails.config.custom.dotenv ?
+        sails.config.custom.dotenv.SOLID_PANCAKE_URL :
+        process.env.SOLID_PANCAKE_URL,
     };
   }
 }
