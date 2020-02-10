a06ec4b43d47fa2c571b7dd418ce8a43022717ef
thefirstspine <teddy@coretizone.com>
2020-02-10 09:47:52 +0100

    Added dotenv support

--- config/env/production.js
+++ config/env/production.js
@@ -372,6 +372,7 @@ module.exports = {
   custom: {
     baseUrl: 'https://example.com',
     internalEmailAddress: 'support@example.com',
+    dotenv: require('dotenv').config().parsed,
 
     // mailgunDomain: 'mg.example.com',
     // mailgunSecret: 'key-prod_fake_bd32301385130a0bafe030c',
