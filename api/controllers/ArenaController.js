625672d4fd589228e02cd994ebea666b5f68f5f6
thefirstspine <teddy@coretizone.com>
2020-03-05 09:18:34 +0100

    Added access token to layout config

--- api/controllers/ArenaController.js
+++ api/controllers/ArenaController.js
@@ -11,7 +11,7 @@ module.exports = {
     return res.view(
       'pages/arena.ejs',
       {
-        ...await sails.helpers.layoutConfig(),
+        ...await sails.helpers.layoutConfig(req.session.access_token),
         windowsVersion: await sails.helpers.getAppVersion('windows'),
         macosVersion: await sails.helpers.getAppVersion('macos'),
         linuxVersion: await sails.helpers.getAppVersion('linux'),
