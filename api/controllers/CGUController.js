3b76802f1e861790902f3ddd27736736e31766a5
thefirstspine <teddy@coretizone.com>
2020-03-05 13:08:19 +0100

    Added CGU

---
+++ api/controllers/CGUController.js
@@ -0,0 +1,20 @@
+/**
+ * CGUController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+  async index(req, res) {
+    return res.view(
+      'pages/cgu.ejs',
+      {
+        ...await sails.helpers.layoutConfig(req.session.access_token),
+      }
+    );
+  }
+
+};
+
