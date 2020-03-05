1ba33dfe0cd02bafcd8ea5a221a4123e077cad9c
thefirstspine <teddy@coretizone.com>
2020-03-05 23:07:29 +0100

    Added footer

--- api/helpers/layout-config.js
+++ api/helpers/layout-config.js
@@ -13,6 +13,15 @@ module.exports = {
       solidPancakeUrl: sails.config.custom.dotenv ?
         sails.config.custom.dotenv.SOLID_PANCAKE_URL :
         process.env.SOLID_PANCAKE_URL,
+      footer: {
+        news: await sails.models.news.find({
+          limit: 3,
+          sort: 'createdAt DESC',
+          where: {
+            language: 'fr',
+          },
+        }),
+      }
     };
     
     // On provided access token
