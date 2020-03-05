5f292401273e2551d7912778e38e62c193f2f0c1
thefirstspine <teddy@coretizone.com>
2020-03-05 09:10:47 +0100

    Added news

--- api/controllers/HomepageController.js
+++ api/controllers/HomepageController.js
@@ -11,10 +11,15 @@ module.exports = {
     return res.view(
       'pages/homepage.ejs',
       {
-        ...await sails.helpers.layoutConfig(),
+        ...await sails.helpers.layoutConfig(req.session.access_token),
         windowsVersion: await sails.helpers.getAppVersion('windows'),
         macosVersion: await sails.helpers.getAppVersion('macos'),
         linuxVersion: await sails.helpers.getAppVersion('linux'),
+        news: await sails.models.news.find({
+          limit: 3,
+          sort: 'createdAt DESC',
+        }),
+        currentCycle: await sails.helpers.getCycle('current'),
       }
     );
   }
---
+++ api/models/News.js
@@ -0,0 +1,35 @@
+/**
+ * News.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  tableName: 'news',
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    slug: { type: 'string', unique: true, required: true },
+    title: { type: 'string' },
+    text: { type: 'string' },
+    createdAt: { type: 'number', autoCreatedAt: true },
+    updatedAt: { type: 'number', autoUpdatedAt: true },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
