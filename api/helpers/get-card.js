836d71cc2f702bc7878ea82d033f9f9cef3fab36
thefirstspine <teddy@coretizone.com>
2020-03-06 09:22:30 +0100

    Added true cards in home page

---
+++ api/helpers/get-card.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+
+  inputs: {
+    id: {
+      type: 'string',
+      required: true
+    }
+  },
+
+  fn: async function(inputs, exits) {
+    try {
+      const baseUrl = sails.config.custom.dotenv ?
+        sails.config.custom.dotenv.REST_URL :
+        process.env.REST_URL;
+      const url = `${baseUrl}/rest/cards/${inputs.id}`;
+      const result = await fetch(url);
+      const resultJson = await result.json();
+      return exits.success(resultJson);
+    } catch (e) {
+      console.log(e);
+      return exits.success(false);
+    }
+  }
+}
