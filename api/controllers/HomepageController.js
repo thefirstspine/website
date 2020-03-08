e4dc99793cf0bd6f40d67edb3d1aa16b74e55bbb
thefirstspine <teddy@coretizone.com>
2020-03-08 14:11:14 +0100

    New cards of the day

--- api/controllers/HomepageController.js
+++ api/controllers/HomepageController.js
@@ -57,7 +57,56 @@ module.exports = {
     });
 
     // Cards of the day
-    const cardsOfTheDay = ['the-fox', 'veneniagora', 'deadly-viper'];
+    const cardsOfTheDay = [
+      'the-tower',
+      'deadly-viper',
+      'summoner',
+      'hunter',
+      'thunder',
+      'putrefaction',
+      'ruin',
+      'heal',
+      'reconstruct',
+      'replacement',
+      'smoky-totem',
+      'barbers',
+      'gargoyle',
+      'banshee',
+      'the-fox',
+      'veneniagora',
+      'soul-of-a-sacrified-hunter',
+      'ether',
+      'conjurer',
+      'sorcerer',
+      'the-wall',
+      'shadows-banner',
+      'insanes-run',
+      'growing-oak',
+      'monstrous-portal',
+      'jester',
+      'paladin-of-the-ninth-desert',
+      'volkha',
+      'insanes-echo',
+      'alter-the-fate',
+      'fire',
+      'achieve',
+      /*
+      These cards are digital-only.
+      TODO: Think about to add them
+      'curse-of-mara',
+      'eternity-gift',
+      'hunter-souvenir',
+      'conjurer-souvenir',
+      'summoner-souvenir',
+      'sorcerer-souvenir',
+      'snow-man-s-present',
+      'great-old-egg',
+      'juvenile-great-old',
+      'great-old',
+      'ovil',
+      'applicant',
+      */
+    ];
 
     return res.view(
       'pages/homepage.ejs',
