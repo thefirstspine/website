b32a59deb61f95df327e6952ad84a37e2a0592a9
thefirstspine <teddy@coretizone.com>
2020-03-20 18:33:19 +0100

    Managed errors

--- api/controllers/HomepageController.js
+++ api/controllers/HomepageController.js
@@ -8,7 +8,7 @@
 module.exports = {
 
   async index(req, res) {
-    if (Date.now() < (new Date("2020-04-03 17:00:00")).getTime()) {
+    if (false && Date.now() < (new Date("2020-04-03 17:00:00")).getTime()) {
       return res.view(
         'pages/wait.ejs',
         {
