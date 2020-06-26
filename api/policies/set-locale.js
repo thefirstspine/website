12d040c3263e62a023f45750d277cf33bce85e3c
thefirstspine <teddy@coretizone.com>
2020-06-26 17:20:46 +0200

    Set language acconrding to accept-language header

--- api/policies/set-locale.js
+++ api/policies/set-locale.js
@@ -1,8 +1,19 @@
 const fetch = require('node-fetch');
 
-module.exports = async function (req, res, proceed) {
-  console.log(req.session.locale);
-  const locale = req.session.locale ? req.session.locale : 'fr';
+module.exports = async (req, res, proceed) => {
+  const getLocalFromPrefs = (accept) => {
+    if (!accept) {
+      return 'en'; // default on non preferences
+    }
+    const matches = accept.match(/[a-z]{2,8}/gm);
+    const allowed = ['fr', 'en'];
+    if (matches[0] && allowed.includes(matches[0])) {
+      return matches[0];
+    }
+    return 'en'; // cannot find prefs here
+  };
+
+  const locale = req.session.locale ? req.session.locale : getLocalFromPrefs(req.headers['accept-language']);
   req.session.locale = locale;
   req.setLocale(locale);
   return proceed();
