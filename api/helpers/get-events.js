af8293473c46f26d530fc021a454c9da735e59d8
thefirstspine <teddy@coretizone.com>
2020-11-22 19:23:17 +0100

    Handled events from calendar service

---
+++ api/helpers/get-events.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+
+  inputs: {
+  },
+
+  fn: async function(inputs, exits) {
+    try {
+      const baseUrl = process.env.CALENDAR_URL;
+      const date = (new Date()).toISOString();
+      const url = `${baseUrl}/events?filter=datetimeTo||gt||${date}`;
+      const result = await fetch(url);
+      const resultJson = await result.json();
+      return exits.success(resultJson);
+    } catch (e) {
+      console.log(e);
+      return exits.success(false);
+    }
+  }
+}
