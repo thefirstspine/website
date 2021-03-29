231eda48a5145f894ed188b16e2f9d244d13ec86
thefirstspine <teddy@coretizone.com>
2021-03-29 23:30:19 +0200

    Added helper

---
+++ api/helpers/create-room.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+
+  inputs: {
+    name: {
+      type: 'string',
+      required: true
+    }
+  },
+
+  fn: async function(inputs, exits) {
+    try {
+      const url = `${process.env.ROOMS_URL}/api/subjects/arena/rooms`;
+      const response = await fetch(url, {
+        body: JSON.stringify({
+          name: inputs.name,
+          senders: [],
+        }),
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Client-Cert': Buffer.from(process.env.ROOMS_PUBLIC_KEY.replace(/\\n/gm, '\n')).toString('base64'),
+        },
+      });
+      const resultJson = await response.json();
+      return exits.success(resultJson);
+    } catch (e) {
+      console.log(e);
+      return exits.success(false);
+    }
+  }
+}
