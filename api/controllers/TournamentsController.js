e5414dfc6a7f7f1bb60d51d37dbf99b4020907d9
thefirstspine <teddy@coretizone.com>
2021-03-30 10:45:24 +0200

    Added participants to tournaments

--- api/controllers/TournamentsController.js
+++ api/controllers/TournamentsController.js
@@ -5,6 +5,8 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const { default: fetch } = require("node-fetch");
+
 module.exports = {
 
   async viewPage(req, res) {
@@ -12,16 +14,38 @@ module.exports = {
     if (!tournament) {
       return res.notFound();
     }
+    
+    const dataToView = {
+      tournament,
+      ...await sails.helpers.layoutConfig(req.user_id),
+    }
 
-    const registration = req.user_id ? await sails.models.tournamentregistration.findOne({tournamentId: tournament.id, userId: req.user_id}) : null;
+    if (req.user_id) {
+      const registration = await sails.models.tournamentregistration.findOne({tournamentId: tournament.id, userId: req.user_id});
+      const matches = await sails.models.tournamentmatch.find({tournamentId: tournament.id});
+      const cachedWizards = {};
+      const matchesConsolidated = await Promise.all(matches.map(async (match) => {
+        if (typeof cachedWizards[match.userId1] === "undefined") {
+          const wizard1Response = await fetch(`${process.env.ARENA_REALMS_URL.replace('{realm}', 'sanctuaire')}/wizard/${match.userId1}`);
+          const wizard1Json = await wizard1Response.json();
+          cachedWizards[match.userId1] = wizard1Json;
+        }
+        match.wizard1 = cachedWizards[match.userId1];
+        if (typeof cachedWizards[match.userId2] === "undefined") {
+          const wizard2Response = await fetch(`${process.env.ARENA_REALMS_URL.replace('{realm}', 'sanctuaire')}/wizard/${match.userId2}`);
+          const wizard2Json = await wizard2Response.json();
+          cachedWizards[match.userId2] = wizard2Json;
+        }
+        match.wizard2 = cachedWizards[match.userId2];
+        return match;
+      }));
+      dataToView.registration = registration;
+      dataToView.matches = matchesConsolidated;
+    }
 
     return res.view(
       'pages/tournament',
-      {
-        tournament,
-        registration,
-        ...await sails.helpers.layoutConfig(req.user_id),
-      }
+      dataToView
     );
   },
 
