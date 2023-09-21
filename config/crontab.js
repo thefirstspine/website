879af50bb3270798fe4147bb0554670d4e7091c9
The First Spine <59167793+thefirstspine@users.noreply.github.com>
2023-09-21 09:11:10 +0000

    Removed cron

--- config/crontab.js
+++ config/crontab.js
@@ -9,21 +9,9 @@ module.exports.crontab = {
   
   crons:function() {
     return [
-      {interval: '0 0 * * *', method: 'refer-a-friend-send-emails'},
-      {interval: '* * * * *', method: 'tournaments-open'},
-      {interval: '* * * * *', method: 'tournaments-watch-match'},
     ];
   },
   
   // declare the method mytest
   // and add it in the crons function
-  'refer-a-friend-send-emails': function() {
-    require('../crontab/refer-a-friend-send-emails.js').run();
-  },
-  'tournaments-open': function() {
-    require('../crontab/tournaments-open.js').run();
-  },
-  'tournaments-watch-match': function() {
-    require('../crontab/tournaments-watch-match.js').run();
-  },
 };
