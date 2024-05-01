307fbf86e588763a402dfb5b13c37e265e05183f
TheFirstSpine <teddy@thefirstspine.fr>
2024-05-01 20:13:11 +0200

    feature(logs): install new logs service

--- config/log.js
+++ config/log.js
@@ -10,6 +10,36 @@
  * https://sailsjs.com/docs/concepts/logging
  */
 
+const { LogsService } = require('@thefirstspine/logs/lib/logs.service');
+const logsService = new LogsService({
+  console: true,
+  loggly: (process.env.LOGS_LOGGLY_SUBDOMAIN != undefined && process.env.LOGS_LOGGLY_TOKEN != undefined),
+  datadog: (process.env.LOGS_DD_API_KEY != undefined),
+});
+
+const logger = {
+  warn: (...params) => {
+    console.log('Log "warn" from website', { params });
+    logsService.warning('Log "warn" from website', { params });
+  },
+  error: (...params) => {
+    console.log('Log "error" from website', { params });
+    logsService.error('Log "error" from website', { params });
+  },
+  debug: (...params) => {
+    console.log('Log "debug" from website', { params });
+    logsService.info('Log "debug" from website', { params });
+  },
+  info: (...params) => {
+    console.log('Log "info" from website', { params });
+    logsService.info('Log "info" from website', { params });
+  },
+  log: (...params) => {
+    console.log('Log "log" from website', { params });
+    logsService.info('Log "log" from website', { params });
+  },
+}
+
 module.exports.log = {
 
   /***************************************************************************
@@ -25,5 +55,6 @@ module.exports.log = {
   ***************************************************************************/
 
   // level: 'info'
+  custom: logger,
 
 };
